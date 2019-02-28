340a7c0cd15e8b8527f71e925328fca793c04073
willbritt <47224666+willbritt@users.noreply.github.com>
2019-02-28 01:06:01 -0500

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-var dataset = [ "Lion", "Zebra", "Giraffe", "Hippo"];
-d3.select("body").append("h1").text("Data Driven Document HW");
-d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d) {return d; });
