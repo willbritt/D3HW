38f3c313d048d2ea3beb61001a4edec0494d2562
willbritt <47224666+willbritt@users.noreply.github.com>
2019-02-28 01:21:04 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 var dataset = [ "Lion", "Zebra", "Giraffe", "Hippo"];
-d3.select("body").append("h1").text("Data Driven Document HW");
+d3.select("body").append("p").text("Data Driven Document HW");
 d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d) {return d; });
